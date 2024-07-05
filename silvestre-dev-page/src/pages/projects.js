de48df8fbe528b3b3217b2dd7b3cfba4c63ebe61
SilvestreL <lucassilvestreadv@gmail.com>
2024-07-05 08:59:37 -0300

    Create projects.js

---
+++ silvestre-dev-page/src/pages/projects.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const projects = () => {
+  return (
+    <div>Projeto Marketplace
+    Bem-vindo ao Projeto Marketplace! Este é um projeto desenvolvido para criar uma plataforma de comércio eletrônico onde os usuários podem comprar e vender produtos. Este README fornece uma visão geral das tecnologias utilizadas, como configurar o ambiente de desenvolvimento e executar o projeto.
+    
+    Visão Geral do Projeto
+    O Projeto Marketplace é uma aplicação full-stack que permite aos usuários navegar, listar e comprar produtos. Ele foi desenvolvido utilizando uma variedade de tecnologias modernas para garantir uma experiência de usuário fluida e um back-end robusto.
+    
+    Tecnologias Utilizadas
+    Front-end
+    
+    ReactJS: Utilizado para a construção da interface do usuário. ReactJS é uma biblioteca JavaScript eficiente e flexível para criar interfaces de usuário reutilizáveis e componentes interativos.
+    Framer Motion: Biblioteca de animação para React, utilizada para adicionar animações fluidas e interações aos componentes da aplicação, proporcionando uma experiência visual atraente.
+    Vite: Ferramenta de build que oferece um ambiente de desenvolvimento rápido e eficiente para projetos React. Vite melhora a performance e facilita o desenvolvimento com HMR (Hot Module Replacement).
+    Back-end
+    
+    NodeJS: Plataforma de execução de JavaScript do lado do servidor, utilizada para criar um back-end eficiente e escalável.
+    Express: Framework para NodeJS que simplifica a construção de APIs RESTful. Utilizado para gerenciar as rotas e middlewares do servidor.
+    Banco de Dados
+    
+    MySQL: Sistema de gerenciamento de banco de dados relacional utilizado para armazenar informações dos produtos, usuários e transações. Escolhido por sua performance, escalabilidade e robustez.
+    Outras Tecnologias
+    
+    API Rest: Arquitetura utilizada para comunicação entre o front-end e o back-end, facilitando a manipulação de dados entre o cliente e o servidor.
+    GIT/GitHub: Sistema de controle de versão usado para gerenciar o código-fonte do projeto. GitHub é utilizado para armazenar e colaborar no repositório.
+    Jest: Framework de teste utilizado para garantir a qualidade do código e a confiabilidade do sistema. Jest facilita a criação de testes unitários para componentes e funções.</div>
+  )
+}
+
+export default projects
